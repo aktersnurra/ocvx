c0419bd08168a6eafe71e26c70d02146fb3058d7
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2026-02-17 23:06:21 +0100

    Update test to use alcotest

--- test/test_osqp.ml
+++ test/test_osqp.ml
@@ -1,6 +1,4 @@
-open Ctypes
-open Foreign
-open Result.Syntax
+let settings = { Osqp.default_settings with verbose = false }
 
 let base_problem () =
   let p = [| [| 4.0; 1.0 |]; [| 1.0; 2.0 |] |] in
@@ -10,55 +8,49 @@ let base_problem () =
   let u = [| 1.0; 0.7; 0.7 |] in
   (p, q, a, l, u)
 
+let unwrap = function
+  | Ok x -> x
+  | Error _ -> Alcotest.fail "unexpected error"
+
 let test_solve () =
   let p, q, a, l, u = base_problem () in
-  let* t = Osqp.setup ~p ~q ~a ~l ~u () in
-  let* sol = Osqp.solve t in
-  Array.iteri (fun i v -> Printf.printf "x[%d] = %f\n" i v) sol.x;
-  Ok ()
+  let t = Osqp.setup ~settings ~p ~q ~a ~l ~u () |> unwrap in
+  let sol = Osqp.solve t |> unwrap in
+  Alcotest.(check (float 1e-2)) "x[0]" 0.3 sol.x.(0);
+  Alcotest.(check (float 1e-2)) "x[1]" 0.7 sol.x.(1)
 
 let test_warm_start () =
   let p, q, a, l, u = base_problem () in
-  let* t = Osqp.setup ~p ~q ~a ~l ~u () in
-  let* sol = Osqp.solve t in
-  let* () = Osqp.warm_start t ~x:sol.x ~y:sol.y () in
-  let* sol2 = Osqp.solve t in
-  Array.iteri (fun i v -> Printf.printf "x[%d] = %f\n" i v) sol2.x;
-  Ok ()
+  let t = Osqp.setup ~settings ~p ~q ~a ~l ~u () |> unwrap in
+  let sol = Osqp.solve t |> unwrap in
+  Osqp.warm_start t ~x:sol.x ~y:sol.y () |> unwrap;
+  let sol2 = Osqp.solve t |> unwrap in
+  Alcotest.(check (float 1e-2)) "x[0]" sol.x.(0) sol2.x.(0);
+  Alcotest.(check (float 1e-2)) "x[1]" sol.x.(1) sol2.x.(1)
 
 let test_update_vectors () =
   let p, q, a, l, u = base_problem () in
-  let* t = Osqp.setup ~p ~q ~a ~l ~u () in
-  let* _ = Osqp.solve t in
-  let* () = Osqp.update_vectors t ~q:[| 2.0; 1.0 |] () in
-  let* sol = Osqp.solve t in
-  Array.iteri (fun i v -> Printf.printf "x[%d] = %f\n" i v) sol.x;
-  Ok ()
+  let t = Osqp.setup ~settings ~p ~q ~a ~l ~u () |> unwrap in
+  let _ = Osqp.solve t |> unwrap in
+  Osqp.update_vectors t ~q:[| 2.0; 1.0 |] () |> unwrap;
+  let sol = Osqp.solve t |> unwrap in
+  Alcotest.(check (float 1e-2)) "x[0]" 0.3 sol.x.(0);
+  Alcotest.(check (float 1e-2)) "x[1]" 0.7 sol.x.(1)
 
 let test_update_matrices () =
   let p, q, a, l, u = base_problem () in
-  let* t = Osqp.setup ~p ~q ~a ~l ~u () in
-  let* _ = Osqp.solve t in
+  let t = Osqp.setup ~settings ~p ~q ~a ~l ~u () |> unwrap in
+  let _ = Osqp.solve t |> unwrap in
   let p' = [| [| 6.0; 1.0 |]; [| 1.0; 2.0 |] |] in
-  let* () = Osqp.update_matrices t ~p:p' () in
-  let* sol = Osqp.solve t in
-  Array.iteri (fun i v -> Printf.printf "x[%d] = %f\n" i v) sol.x;
-  Ok ()
+  Osqp.update_matrices t ~p:p' () |> unwrap;
+  let sol = Osqp.solve t |> unwrap in
+  Alcotest.(check bool) "solution non-empty" true (Array.length sol.x > 0)
 
 let () =
-  Printf.printf "\n=== Test: solve ===\n";
-  (match test_solve () with
-   | Error _ -> Printf.printf "FAIL\n"
-   | Ok () -> Printf.printf "PASS\n");
-  Printf.printf "\n=== Test: warm_start ===\n";
-  (match test_warm_start () with
-   | Error _ -> Printf.printf "FAIL\n"
-   | Ok () -> Printf.printf "PASS\n");
-  Printf.printf "\n=== Test: update_vectors ===\n";
-  (match test_update_vectors () with
-   | Error _ -> Printf.printf "FAIL\n"
-   | Ok () -> Printf.printf "PASS\n");
-  Printf.printf "\n=== Test: update_matrices ===\n";
-  (match test_update_matrices () with
-   | Error _ -> Printf.printf "FAIL\n"
-   | Ok () -> Printf.printf "PASS\n")
+  Alcotest.run "OSQP"
+    [
+      ("solve", [ Alcotest.test_case "basic" `Quick test_solve ]);
+      ("warm_start", [ Alcotest.test_case "matches initial solve" `Quick test_warm_start ]);
+      ("update_vectors", [ Alcotest.test_case "change q" `Quick test_update_vectors ]);
+      ("update_matrices", [ Alcotest.test_case "change p" `Quick test_update_matrices ]);
+    ]
